c4dcb540be97c49134aebb1e27abd94e0a53d445
Swan <swansantanas@gmail.com>
2021-12-15 11:25:02 -0300

    Projeto spring boot
    Projeto com as configurações iniciais definido pelo spring boot

---
+++ src/main/java/com/escolacrud/escolacrud/EscolacrudApplication.java
@@ -0,0 +1,13 @@
+package com.escolacrud.escolacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolacrudApplication.class, args);
+	}
+
+}
